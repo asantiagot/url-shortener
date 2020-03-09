8e12c91e41e5401a56a5ce2a2beaa2ccd49ed95c
Jose Santiago <josantorres93@gmail.com>
2020-03-08 23:17:20 -0600

    added mongoose schema

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortURL = require('./models/ShortURL');
 const app = express();
 
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.set('view engine', 'ejs');
-app.listen(process.env.port || 5000);
+app.use(express.urlencoded({ extended: false}));
 
 // routing
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+app.post('/shortenUrl', async (req, res) => {
+    console.log(`url is ${req.body.url}`);
+    await ShortURL.create({
+        full: req.body.url
+    });
+    res.redirect('/');
+});
+
+app.listen(process.env.port || 5000);
