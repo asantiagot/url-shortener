83828f0b18939128d62fef90a344b3a04fef0f22
Jose Santiago <josantorres93@gmail.com>
2020-03-12 09:10:39 -0600

    url shortener ready; handles dynamic redirects

--- server.js
+++ server.js
@@ -19,10 +19,22 @@ app.get('/', async (req, res) => {
 
 app.post('/shortenUrl', async (req, res) => {
     console.log(`url is ${req.body.url}`);
-    await ShortURL.create({
-        full: req.body.url
-    });
+    const url = await ShortURL.findOne({ full:  req.body.url });
+    if (url == null) {
+        await ShortURL.create({
+            full: req.body.url
+        });
+    }
     res.redirect('/');
 });
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortURL.findOne({ short:  req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+
+    res.redirect(shortUrl.full);
+})
 
 app.listen(process.env.port || 5000);
